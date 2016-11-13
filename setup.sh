5e2d571e261a88da8f47f0df6488b44444816193
Jason McGlade <jmcglade@seek.com.au>
2016-11-13 22:10:02 +1100

    Adding initial .spacemacs config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 git submodule init
 git submodule update
 
-ln -sf $PWD/.config ~/.config
+ln -sfh $PWD/.config ~/.config
+ln -sfh $PWD/.spacemacs ~/.spacemacs
 
-curl -fLo ~/Library/Fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf
+curl -fLo ~/Library/Fonts/Sauce\ Code\ Pro\ Nerd\ Font\ Complete.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Semibold/complete/Sauce%20Code%20Pro%20Semibold%20Nerd%20Font%20Complete.ttf
